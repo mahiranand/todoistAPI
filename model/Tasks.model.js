55b523febb4491d7747be8fbb32f1facedafd487
mahiranand <mahiranand08@gmail.com>
2024-01-23 12:01:48 +0530

    .gitignore

--- model/Tasks.model.js
+++ model/Tasks.model.js
@@ -1,57 +1,39 @@
 import sequelize from "../config/config";
 import { DataTypes } from "sequelize";
 
-const Tasks = sequelize.define(
-  "Tasks",
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-      allowNull: false,
-    },
-    content: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    comment_count: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-    completed: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    due: {
-      type: DataTypes.STRING,
-      defaultValue: null,
-    },
-    label_ids: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      defaultValue: [],
-    },
-    order: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-    priority: {
-      type: DataTypes.INTEGER,
-      defaultValue: 1,
-    },
-    project_id: {
-      type: DataTypes.STRING,
-      defaultValue: null,
-    },
-    section_id: {
-      type: DataTypes.STRING,
-      defaultValue: null,
-    },
-    url: {
-      type: DataTypes.STRING,
-      defaultValue: null,
-    },
+const Tasks = sequelize.define("Tasks", {
+  creator_id: {
+    type: DataTypes.STRING,
+    defaultValue: null
   },
-  {
-    timestamps: false,
+  assignee_id: {
+    type: DataTypes.STRING,
+    defaultValue: null
+  },
+  assigner_id: {
+    type: DataTypes.STRING,
+    defaultValue: null
+  },
+  comment_count: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+  is_completed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    defaultValue: ""
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  due:{
+    type: DataTypes.JSONB,
+    
   }
-);
+},{
+  updatedAt: false,
+});
