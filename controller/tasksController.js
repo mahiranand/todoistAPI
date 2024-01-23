f0aac1ee1c3c1667a2c9f8e1392004723fafdd5a
mahiranand <mahiranand08@gmail.com>
2024-01-23 16:39:23 +0530

    tasks api added

--- controller/tasksController.js
+++ controller/tasksController.js
@@ -1,3 +1,126 @@
+import Tasks from "../model/Tasks.model.js";
+
 export const getActiveTasks = (req, res) => {
-    
+  Tasks.findAll({
+    where: {
+      is_completed: false,
+    },
+  })
+    .then((tasks) => {
+      res.json(tasks);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+};
+
+export const createNewTask = (req, res) => {
+  const task = {
+    ...req.body,
+    due: {
+      date: req.body.due_date,
+      is_recurring: false,
+      string: req.body.due_string,
+      datetime: req.body.due_datetime,
+    },
+    project_id: req.params.projectId,
+    duration: {
+      amount: req.body.duration,
+      unit: req.body.duration_unit,
+    },
+  };
+  Tasks.create(task)
+    .then((task) => {
+      res.json(task);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+};
+
+export const getActiveTaskById = (req, res) => {
+  Tasks.findOne({
+    where: {
+      id: req.params.id,
+      is_completed: false,
+    },
+  })
+    .then((task) => {
+      if (task === null) {
+        res.status(404).json({ message: "Task not found" });
+      } else {
+        res.json(task);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+};
+
+export const updateATask = (req, res) => {
+  Tasks.findByPk(req.params.id)
+    .then((task) => {
+      if (task === null) {
+        res.status(404).json({ message: "Task not found" });
+      } else {
+        return task.update(req.body);
+      }
+    })
+    .then((task) => {
+      res.json(task);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+};
+
+export const closeATask = (req, res) => {
+  Tasks.findByPk(req.params.id)
+    .then((task) => {
+      if (task === null) {
+        res.status(404).json({ message: "Task not found" });
+      } else {
+        return task.update({ is_completed: true });
+      }
+    })
+    .then((task) => {
+      res.json(task);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+};
+
+export const reopenATask = (req, res) => {
+  Tasks.findByPk(req.params.id)
+    .then((task) => {
+      if (task === null) {
+        res.status(404).json({ message: "Task not found" });
+      } else {
+        return task.update({ is_completed: false });
+      }
+    })
+    .then((task) => {
+      res.json(task);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+};
+
+export const deleteATask = (req, res) => {
+  Tasks.findByPk(req.params.id)
+    .then((task) => {
+      if (task === null) {
+        res.status(404).json({ message: "Task not found" });
+      } else {
+        return task.destroy();
+      }
+    })
+    .then(() => {
+      res.end();
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
 };
--- route/Tasks.route.js
+++ route/Tasks.route.js
@@ -1,14 +1,22 @@
 import express from "express";
-import { getActiveTasks } from "../controller/tasksController.js";
+import {
+  createNewTask,
+  updateATask,
+  getActiveTasks,
+  getActiveTaskById,
+  closeATask,
+  reopenATask,
+  deleteATask,
+} from "../controller/tasksController.js";
 
 const router = express.Router();
 
 router.get("/", getActiveTasks);
-// router.post("/:projectId", createNewTask);
-// router.get("/:id", getActiveTaskById);
-// router.put("/:id", updateATask);
-// router.put("/:id/close", closeATask);
-// router.put("/:id/reopen", reopenATask);
-// router.delete("/:id", deleteATask);
+router.post("/:projectId", createNewTask);
+router.get("/:id", getActiveTaskById);
+router.put("/:id", updateATask);
+router.put("/:id/close", closeATask);
+router.put("/:id/reopen", reopenATask);
+router.delete("/:id", deleteATask);
 
 export default router;
